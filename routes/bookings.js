913a600ee3e263ca33d875ea20e327815e34cdff
Dzmitry <ignatovich.dm@gmail.com>
2024-08-12 16:39:50 +0200

    added booking routes

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
+const authenticateToken = require('../middleware/authMiddleware');
+const bookingController = require('../controllers/booking')
 
-router.post('/bookings', function (req, res, next) {
-  res.send('respond with a resource')
-})
-
-router.get('/bookings', function (req, res, next) {
-  res.send('respond with a resource')
-})
-
-router.get('/bookings/:id', function (req, res, next) {
-  res.send('respond with a resource')
-})
-
-router.put('/bookings/:id', function (req, res, next) {
-  res.send('respond with a resource')
-})
+router.post('/bookings', authenticateToken, bookingController.createBooking);
+router.get('/bookings', authenticateToken, bookingController.getAllBookings);
+router.get('/bookings/:id', authenticateToken, bookingController.getBookingById);
+router.put('/bookings/:id', authenticateToken, bookingController.updateBooking);
+router.delete('/bookings/:id', authenticateToken, bookingController.deleteBooking);
